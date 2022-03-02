553bcd59f8408bf0588d85b0fc815642f52a6451
petkova <petkova@omicronconsulting.it>
2022-03-02 16:05:20 +0100

    Init commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fileSystemApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fileSystemApp
+ */
+angular.module('fileSystemApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
